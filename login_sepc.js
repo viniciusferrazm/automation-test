b8051624c54c4cf88b1efb48c4819b4255d1f1e4
viniciusferrazm <83127000+viniciusferrazm@users.noreply.github.com>
2021-04-26 08:40:18 -0300

    Update login_sepc.js

--- login_sepc.js
+++ login_sepc.js
@@ -1,17 +1,17 @@
 context('Login no website da AMAZON', () => {
     it('Acessar o site', () => {
-        cy.visit('https://www.amazon.com.br/')
-        cy.get('.nav-line-1-container').click({ force: true })
-        cy.get('#ap_email').type("emailtesteqa1@gmail.com")
-        cy.get('.a-button-input').click()
-        cy.get("#ap_password").type("Teste@123")
-        cy.get('.a-button-input').click()
-        cy.wait(300)   
+        cy.visit('https://www.amazon.com.br/') 
+        cy.get('.nav-line-1-container').click({ force: true }) //click no botão "Faça seu login"
+        cy.get('#ap_email').type("emailtesteqa1@gmail.com") //informa o email no campo de email
+        cy.get('.a-button-input').click() //click no botão para prosseguir
+        cy.get("#ap_password").type("Teste@123") //informa a senha no campo de senha
+        cy.get('.a-button-input').click() //click no botão para prosseguir
+        cy.wait(300)   //delay para quando a tela leva mais tempo para atualizar
         
     });
     it('Pesquisar um iphone 11', () => { 
-        cy.get('#twotabsearchtextbox').type("Iphone 11")
-        cy.get('#nav-search-submit-button').click({ force:true })
+        cy.get('#twotabsearchtextbox').type("Iphone 11") //informa o texto no campo de pesquisa
+        cy.get('#nav-search-submit-button').click({ force:true }) //click no botão para prosseguir
 
         
     });
